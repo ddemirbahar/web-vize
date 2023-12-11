ea176d2a1b183bda35ac99223f3de482d0fb82bf
ddemirbahar <babardenir421@gmail.com>
2023-12-12 02:49:05 +0300

    commit 3

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,6 +8,7 @@ const routes: RouteRecordRaw[] = [
       { path: '', component: () => import('pages/IndexPage.vue') },
       { path: 'comp1', component: () => import('components/ThePage.vue') },
       { path: 'comp2', component: () => import('components/ThePage2.vue') },
+      { path: 'comp3', component: () => import('components/ThePage3.vue') },
     ],
   },
 
